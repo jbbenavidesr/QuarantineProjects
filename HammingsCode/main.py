eb7f039daddc32deaffb17b3fcef7b7c0577976b
Juan B Benavides <jbbenavidesr@unal.edu.co>
2020-04-22 13:33:38 -0500

    Add main

---
+++ HammingsCode/main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import hamming as ham
+
+Nmax= 20
+p = [0.8, 0.2]
+
+data=[]
+
+for n in range(1, Nmax):
+    l = ham.average_length(ham.group_probabilities(p, n), n)
+    data.append(l)
+
+plt.plot(data)
+plt.show()
