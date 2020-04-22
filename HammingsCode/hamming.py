4e15818922e4fde92d9b1a34650a44995debc700
Juan B Benavides <jbbenavidesr@unal.edu.co>
2020-04-21 22:50:22 -0500

    Refactor: get_history

--- HammingsCode/hamming.py
+++ HammingsCode/hamming.py
@@ -1,34 +1,42 @@
-p = [0.81, 0.09, 0.09, 0.01]
+p1 = [0.81, 0.09, 0.09, 0.01]
+p2 = [0.64, 0.16, 0.16, 0.04]
+p3 = [0.45, 0.35, 0.1, 0.05, 0.05]
 
+def get_history(probabilities):
 
-def hamming(probabilities):
-    probabilities.sort(reverse=True)
-    order = [str(i) for i in range(len(probabilities))]
+    history = [str(i) for i in range(len(probabilities))]
     last = probabilities[-1] + probabilities[-2]
     values = [probabilities[i] for i in range(len(probabilities)-2)] + [last]
     while len(values) >= 2:
-        values.sort()
+        values.sort(reverse=True)
         if last == values[-1]:
             for i in range(len(values)):
-                order[i] += str(i)
+                history[i] += str(i)
 
             for i in range(len(values), len(probabilities)):
-                order[i] += str(len(values)-1)
+                history[i] += str(len(values)-1)
 
         else: 
             ind = values.index(last)
             for i in range(ind):
-                order[i] += str(i)
+                history[i] += str(i)
 
             for i in range(len(values)-1, len(probabilities)):
-                order[i] += str(ind)
+                history[i] += str(ind)
 
             for i in range(ind, len(values)-1):
-                order[i] += str(i+1)
-                
+                history[i] += str(i+1)
+
         last = values[-1] + values[-2]
         values = [values[i] for i in range(len(values)-2)] + [last]
-            
-    return list(zip(probabilities, order))
+    
+    return history
 
-print(hamming(p))
+
+def hamming(probabilities):
+    probabilities.sort(reverse=True)
+    history = get_history(probabilities)
+            
+    return list(zip(probabilities, history))
+
+print(hamming(p1))
