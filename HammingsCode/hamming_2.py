c8da3a79489a83f4ee13e3a9f8f57a0f1f728808
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-06-04 23:11:56 -0500

    Feat: Funtional hamming code for entries that are different

--- HammingsCode/hamming_2.py
+++ HammingsCode/hamming_2.py
@@ -1,26 +1,40 @@
+"""
+My try at making hammings work. It only works if all the entries are different.
+
+For example, [0.4, 0.3, 0.2, 0.1] will work, [0.6, 0.2, 0.1, 0.1] will not
+"""
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-p = [0.6, 0.2, 0.1, 0.05, 0.05]
+import math as m
 
+prob = [0.4, 0.3, 0.2, 0.1]
 
-def get_hamming(prob_list):
-	"""
-	"""
-	ordered_list = sorted(prob_list, reverse=True)
-	hamming = {k:"" for k in ordered_list}
-	i = 0
-	code = ""
+def reduce_list(list_, index):
+    """
+    Takes a list and returns all the items up to the index, and the sum of all others
+    Note to self: [a:b] = [a,b)
+    """
+    temp = [x for x in sorted(list_[0:index], reverse=True)]
+    temp.append(m.fsum(list_[index:]))
+    return temp
 
-	while True:
-		temp_lis = sorted([ordered_list[i], sum(ordered_list[i:])], reverse=True)
+hamming = {}
+done = []
+for p in prob:
+    hamming[index] = ''
 
-		hamming[ordered_list[i]] +='1'
-		for p in ordered_list[i+1:]: hamming[p] += "0"
-		
-		i += 1
-		if i == len(ordered_list):
-			break
+for i in range(1,len(prob)):
+    reduced = reduce_list(prob, i)
+    reduced_clean = [r for r in reduced if r not in done] # Not sure why this works, but is necessary
 
-	print(hamming) 
+    for r in reduced_clean:
+        if r in prob:
+            if r not in done:
+                hamming[str(r)] += '1'
+                done.append(r)
 
-get_hamming(p)
+            for p in prob:
+                if p != r and p not in done:
+                    hamming[str(p)] += '0'
+
+print(hamming)
